bbe8473f64a089bc508801de40fb1e0d5b8114b6
Shefalidew <shefali.dew97@gmail.com>
2022-01-21 15:58:31 +0100

    Update transaction_list.dart
    Scrolling function added

--- lib/widgets/transaction_list.dart
+++ lib/widgets/transaction_list.dart
@@ -9,48 +9,50 @@ class TransactionList extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Column(
-        children: transactions.map((tx) {
-          return Card(
-              child: Row(
-            // mainAxisAlignment: MainAxisAlignment.start,
-            children: [
-              Container(
-                margin: EdgeInsets.symmetric(
-                  vertical: 10,
-                  horizontal: 15,
-                ),
-                decoration: BoxDecoration(
-                  border: Border.all(
-                    color: Colors.purple,
-                    width: 2,
+    return Container(height: 300,
+      child: SingleChildScrollView(
+        child: Column(
+          children: transactions.map((tx) {
+            return Card(
+                child: Row(
+              // mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                Container(
+                  margin: EdgeInsets.symmetric(
+                    vertical: 10,
+                    horizontal: 15,
+                  ),
+                  decoration: BoxDecoration(
+                    border: Border.all(
+                      color: Colors.purple,
+                      width: 2,
+                    ),
+                  ),
+                  padding: EdgeInsets.all(10),
+                  child: Text(
+                    '\$ ' + tx.amount.toString(),
+                    style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 20,
+                        color: Colors.purple),
                   ),
                 ),
-                padding: EdgeInsets.all(10),
-                child: Text(
-                  '\$ ' + tx.amount.toString(),
-                  style: TextStyle(
-                      fontWeight: FontWeight.bold,
-                      fontSize: 20,
-                      color: Colors.purple),
-                ),
-              ),
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(tx.title,
-                      style:
-                          TextStyle(fontSize: 20, fontWeight: FontWeight.w800)),
-                  Text(
-                    DateFormat.yMd().add_jm().format(tx.date),
-                    style: TextStyle(color: Colors.grey[700]),
-                  ),
-                ],
-              )
-            ],
-          ));
-        }).toList(),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(tx.title,
+                        style:
+                            TextStyle(fontSize: 20, fontWeight: FontWeight.w800)),
+                    Text(
+                      DateFormat.yMd().add_jm().format(tx.date),
+                      style: TextStyle(color: Colors.grey[700]),
+                    ),
+                  ],
+                )
+              ],
+            ));
+          }).toList(),
+        ),
       ),
     );
   }
